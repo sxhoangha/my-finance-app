3e9e2ed8b1c919a0a26d2255b49d6a57bce40c38
hha <hha@opentext.com>
2024-01-07 17:05:18 -0500

    Add jest-html-reporter for test reporting

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,16 @@ module.exports = {
   transform: {
     "^.+\\.(t|j)sx?$": "@swc/jest",
   },
+  collectCoverage: true,
+  coverageReporters: ["text", "html", "lcov"],
+  coverageDirectory: "<rootDir>/coverage/",
+  reporters: [
+    "default",
+    [
+      "jest-html-reporter",
+      {
+        pageTitle: "Test Report",
+      },
+    ],
+  ],
 };
